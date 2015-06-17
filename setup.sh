fdc5cdd9d43988a2f0b747fe328719ab05aa316e
Idan Yalovich <idan@ystudiodesign.com>
2015-06-17 19:09:06 +0300

    Script.

---
+++ setup.sh
@@ -0,0 +1,21 @@
+sudo apt-get update
+
+sudo apt-get install apache2
+sudo apt-get install php5
+sudo apt-get install mysql-server
+sudo apt-get install mysql-client
+
+sudo apt-get install php5-mysql
+
+sudo a2enmod rewrite
+sudo service apache2 restart
+
+# installing GIT
+sudo apt-get install git
+
+sudo chown -R root:www-data /var/www
+sudo chmod -R 775 /var/www
+
+sudo usermod -a -G www-data ubuntu
+
+ln -s /var/www/ ~/www
